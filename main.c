fa6f564c8cc60f9a94ab70919d7f4c5b50fd0525
medevag <michaeledevag@gmail.com>
2017-01-18 21:46:58 +0100

    c
    x

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-// Pulseaudio is on. Now bluetooth should be initialized.
-int status = 0;
-status = system("/home/pi/smart-car-code/./smart-car-bluetooth");
+int main(void)
+{
+	// Pulseaudio is on. Now bluetooth should be initialized.
+	int status = 0;
+	status = system("/home/pi/smart-car-code/./smart-car-bluetooth");
+]
