a5231ed78914394b4b69e32c26cfe9378a77b306
medevag <michaeledevag@gmail.com>
2017-01-18 21:42:05 +0100

    a

--- main.c
+++ main.c
@@ -2,15 +2,4 @@
 
 
 // Pulseaudio is on. Now bluetooth should be initialized.
-int status;
-
-status = system("echo "***************************************"");
-status = system("echo "* Launching Bluetooth connection code *"");
-status = system("echo "***************************************"");
-
-
-status = system(/home/pi/smart-car-code/./smart-car-bluetooth);
-
-status = system("echo "*****************************"");
-status = system("echo "* Bluetooth connection done *"");
-status = system("echo "*****************************"");
+int status = system(/home/pi/smart-car-code/./smart-car-bluetooth);
