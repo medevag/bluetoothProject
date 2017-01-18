cbb320ff25c504038496506b257a9509f5192b9f
medevag <michaeledevag@gmail.com>
2017-01-18 21:42:35 +0100

    c

--- main.c
+++ main.c
@@ -2,4 +2,4 @@
 
 
 // Pulseaudio is on. Now bluetooth should be initialized.
-int status = system(/home/pi/smart-car-code/./smart-car-bluetooth);
+int status = system("/home/pi/smart-car-code/./smart-car-bluetooth");
