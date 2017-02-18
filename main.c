5494c10cd0aa89f0a0d36fd244a74afae27d7588
medevag <michaeledevag@gmail.com>
2017-02-18 08:57:46 +0000

    hej

--- main.c
+++ main.c
@@ -2,19 +2,52 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
+/*
+BluetoothPin is pin number 11
+GPIO 0 WiringPi 0
+
+ShutdownPin is pin number 13
+GPIO 2 WiringPi 2
+*/
+
+#define SHUTDOWN_REQUESTED     	 (0)
+#define BLUETOOTH_REQUESTED      (1)
+#define SHUTDOWN_PIN  		 (2)
+#define BLUETOOTH_PIN 		 (0)
+
 int main(void)
 {
-	int status = 0, bluetoothTrigger = 0;
+	// Variables
+	int status = 0, bluetoothTrigger = 3, shutdownTrigger = 3;
 
+	// Set up WiringPi
 	wiringPiSetup ();
-	pinMode(0,OUTPUT);
-	digitalWrite(0,0);
-	sleep(10);
-	pinMode(0, INPUT);
 
-	bluetoothTrigger =  digitalRead (0);
+	// Set up pins
+	// Bluetooth check pin
+	pullUpDnControl(BLUETOOTH_PIN,PUD_DOWN);
+	pinMode(BLUETOOTH_PIN,INPUT);
+
+	// Stutdown init pin
+	pullUpDnControl(SHUTDOWN_PIN,PUD_UP);
+	pinMode(SHUTDOWN_PIN,INPUT);
+
+	// Read GPIO to see if bluetooth visible triggered 
+	bluetoothTrigger = digitalRead(BLUETOOTH_PIN);
+
+	// Read GPIO to see if shutdown should be triggered
+	shutdownTrigger = digitalRead(SHUTDOWN_PIN);
 
 	// Pulseaudio is on. Now bluetooth should be initialized.
-	if(bluetoothTrigger)
-		status = system("/home/pi/smart-car-code/./smart-car-bluetooth");
+	if(bluetoothTrigger == BLUETOOTH_REQUESTED){
+		status = system("/home/pi/bluetoothProject/./smart-car-bluetooth");
+	}
+
+	// Shutdown procedure should begin
+	if(shutdownTrigger == SHUTDOWN_REQUESTED){
+		printf("Shutdown");
+		//status = system("sudo init 0");
+	}
+
+
 }
